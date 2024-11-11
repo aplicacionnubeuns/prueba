abd2859bfaefe7909244851f9f7c0228431a4e02
aplicacionnubeuns <155849705+aplicacionnubeuns@users.noreply.github.com>
2024-11-10 22:36:26 -0500

    Update prueba.sql

--- prueba.sql
+++ prueba.sql
@@ -24,8 +24,7 @@ CREATE TABLE s_cliente
 INSERT INTO s_cliente VALUES ('301', 'Sports,Inc', '540-123-4567','72 High St', 'Harrisonburg', 'VA','US', '22809','EXCELENTE', '12', '1', NULL);
 INSERT INTO s_cliente VALUES ('302', 'Toms Sporting Goods', '540-987-6543','6741 Main St','Harrisonburg', 'VA','US', '22809','MALA', '14', '1', NULL);
 INSERT INTO s_cliente VALUES ('303', 'Athletic Attire', '540-123-6789','54 Market St', 'Harrisonburg', 'VA','US', '22808','BUENA', '14', '1', NULL);
-INSERT INTO s_cliente 
-VALUES ('304', 'Athletics For All', '540-987-1234','286 Main St', 'Harrisonburg', 'VA', 'US', '22808','EXCELENTE', '12', '1', NULL);
+INSERT INTO s_cliente VALUES ('304', 'Athletics For All', '540-987-1234','286 Main St', 'Harrisonburg', 'VA', 'US', '22808','EXCELENTE', '12', '1', NULL);
 INSERT INTO s_cliente VALUES ('305', 'Shoes for Sports', '540-123-9876','538 High St', 'Harrisonburg', 'VA','US', '22809','EXCELENTE', '14', '1', NULL);
 INSERT INTO s_cliente VALUES ('306', 'BJ Athletics', '540-987-9999','632 Water St', 'Harrisonburg', 'VA','US', '22810','POOR', '12', '1', NULL);
 
